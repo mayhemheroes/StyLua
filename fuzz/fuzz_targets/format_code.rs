bace3aede288e5e32a84efcd9bb8c29c373d9c96
Dylan <57853417+4812571@users.noreply.github.com>
2023-04-23 14:42:32 -0400

    stylua fuzzer

---
+++ fuzz/fuzz_targets/format_code.rs
@@ -0,0 +1,17 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+use stylua_lib::Range;
+use stylua_lib::Config;
+use stylua_lib::OutputVerification;
+
+use stylua_lib::format_code;
+
+fuzz_target!(|data: &[u8]| {
+    let lua = String::from_utf8_lossy(&data);
+
+    let _ = format_code(
+
+    );
+});
---
+++ fuzz/fuzz_targets/format_code_basic.rs
@@ -0,0 +1,20 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+use stylua_lib::Range;
+use stylua_lib::Config;
+use stylua_lib::OutputVerification;
+
+use stylua_lib::format_code;
+
+fuzz_target!(|data: &[u8]| {
+    let lua = String::from_utf8_lossy(&data);
+
+    let _ = format_code(
+        lua,
+        Config::default(),
+        None,
+        OutputVerification::None,
+    );
+});
